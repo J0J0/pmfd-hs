9ba7c82127b856173a0bc9eceb1af8c68f6c6402
Johannes Prem <Johannes.Prem@stud.uni-regensburg.de>
2014-09-06 22:13:56 +0200

    add DehnAlgorithm

---
+++ src/DehnAlgorithm.hs
@@ -0,0 +1,91 @@
+
+module DehnAlgorithm
+(
+    dehnAlg
+) where
+
+
+import Control.Arrow ( (&&&), first )
+import Data.List ( intercalate, stripPrefix )
+import Data.List.Split ( splitOn )
+import Data.Maybe ( listToMaybe, mapMaybe )
+import Data.Monoid ( First(..), mconcat )
+
+import PolygonScheme ( Scheme, invSch, reduce )
+import Util ( rotations, slidingBlock )
+
+
+-- | @'dehnAlg' rs w@ applies Dehn's algorithm
+--   (<http://en.wikipedia.org/wiki/Small_cancellation_theory#Dehn.27s_algorithm Wikipedia>)
+--   with relators @rs@ to the word @w@. Note, that @rs@ should /not/
+--   include cyclic permutations because they are generated anyway. Examples:
+--   
+--   > mkSch xs = map toEnum xs :: Scheme
+--   > r  = mkSch [1,1,2,2,3,3]  -- relator of the fundamental group of
+--                               -- "the" non-orientable surface of genus 3
+--   > w1 = mkSch [1,2,2,3,3,1]
+--   > w2 = mkSch [1,2,2,3,3]
+--   > w3 = mkSch [2,2,3,3,1,-3,-3,-2,-2,1,-3,-3,-2,-2,-1,-1,-1,-1]
+--   >
+--   > dehnAlg [r] w1 == []
+--   > dehnAlg [r] w2 == mkSch [-1]
+--   > dehnAlg [r] w3 == []
+dehnAlg :: [Scheme] -> Scheme -> Scheme
+dehnAlg rels sch =
+    dehnAlg' allRels minmaxRL' (reduce sch)
+        where
+            allRels   = prepareRels rels
+            kk        = fst $ head allRels
+            minmaxRL  = foldr (\ (k,_) (x,y) -> (min k x, max k x)) 
+                              (kk,kk) allRels
+            minmaxRL' = first (\ k -> k `div` 2 + 1) minmaxRL
+
+type AllRelators = [(Int,[Scheme])]
+-- [(length of following relators, list of relators)]
+
+-- takes a list of relators and adds all cyclic permutations
+-- and their inverses; note, that we make no attempt to detect
+-- or remove any duplicates here
+prepareRels :: [Scheme] -> AllRelators
+prepareRels = map (length &&& mkRels)
+    where
+        mkRels xs = rotations xs ++ rotations (invSch xs)
+
+-- this is the actual implementation of Dehn's algorithm
+dehnAlg' :: AllRelators -- the relators to use; ensure that all cyclic
+                        -- permutations and their inverses are included!
+         -> (Int,Int)   -- (min. relator length / 2 + 1, max. relator length)
+         -> Scheme      -- word to reduce via the algorithm
+         -> Scheme      -- output word; if this is not empty, the
+                        --              input word is non-trivial
+dehnAlg' _ _ [] = []
+dehnAlg' rels rl@(minRLh,maxRL) sch =
+    let nmax        = min maxRL (length sch)
+        ns          = [nmax, nmax-1 .. minRLh]
+        tryReplaceN = tryReplace rels sch
+    in
+        case mconcat $ map tryReplaceN ns of
+            First Nothing     -> sch
+            First (Just sch') -> dehnAlg' rels rl (reduce sch')
+
+-- @'tryReplace' rels sch n@ tries to replace a subword of length @n@
+-- in @sch@ with a shorter word (which is found by 'tryFind', see below).
+-- It returns @First (Just sch')@ with the new word @sch'@,
+-- or @First Nothing@ if there are no matches.
+tryReplace :: AllRelators -> Scheme -> Int -> First Scheme
+tryReplace rels sch n =
+    First $ listToMaybe $ do
+        subsch  <- slidingBlock n sch
+        relrest <- mapMaybe (tryFind subsch n) rels
+        return $ intercalate (invSch relrest) $ splitOn subsch sch
+
+-- @'tryFind' subsch n (k, rs)@ tries to find the word @subsch@
+-- of length @n@ as a prefix of one of the relators @rs@.
+-- It returns the remaining suffix from the first matching relator,
+-- or Nothing if there are no matches.
+tryFind :: Scheme -> Int -> (Int, [Scheme]) -> Maybe Scheme
+tryFind subsch n (k, rs)              -- subscheme is ...
+    | n > k              =  Nothing   --   longer than relator
+    | n < k `div` 2 + 1  =  Nothing   --   shorter than half of relator
+    | otherwise          =
+        listToMaybe $ mapMaybe (stripPrefix subsch) rs
--- src/Util.hs
+++ src/Util.hs
@@ -6,10 +6,15 @@ module Util
     , equalLength
     , roundToMagnitude
     , filter2
+    , rotations
+    , mapFirstN
+    , slidingBlock
+    , slidingBlock'
 ) where
 
 import Control.Arrow ( (***) )
 import Control.Monad ( join )
+import Data.List ( inits, tails )
 
 
 -- | An analog function to 'first' and 'second' from @Control.Arrow@:
@@ -30,6 +35,11 @@ roundToMagnitude :: (Num a, Ord a, Integral i) => a -> i
 roundToMagnitude x =
     head $ dropWhile ((< x) . fromIntegral) $ iterate (*10) 1
 
+-- | Like 'Prelude.filter' with two predicates.
+--   
+--   > filter p q xs == (filter p xs, filter q xs)
+--   
+--   but 'filter2' traverses the list only once.
 filter2 :: (a -> Bool) -> (a -> Bool) -> [a] -> ([a],[a])
 filter2 _ _ [] = ([],[])
 filter2 p q (x:xs) =
@@ -37,3 +47,88 @@ filter2 p q (x:xs) =
         ys' = if p x then x:ys else ys
         zs' = if q x then x:zs else zs
     in (ys',zs')
+
+-- | Returns all rotations of a list, e. g.:
+-- 
+--   > rotations [1..4] == [[1,2,3,4],[2,3,4,1],[3,4,1,2],[4,1,2,3]]
+rotations :: [a] -> [[a]]
+rotations xs =
+    init $ zipWith (++) (tails xs) (inits xs)
+-- from http://stackoverflow.com/a/7632272
+
+-- | Map a function over the first @n@ elements of a list.
+mapFirstN :: Int -> (a -> a) -> [a] -> [a]
+mapFirstN _ _ []     = []
+mapFirstN 0 _ xs     = xs
+mapFirstN k f (x:xs) = f x : mapFirstN (k-1) f xs
+
+-- | @'slidingBlock' k xs@ returns all contiguous sublists of xs that
+--   consist of exactly k elements, e. g.:
+--   
+--   > slidingBlock 3 [1..5] == [[1,2,3],[2,3,4],[3,4,5]]
+--   
+--   If you already know the length of the list, use 'slidingBlock'' directly.
+slidingBlock :: Int -> [a] -> [[a]]
+slidingBlock slen xs = slidingBlock' (length xs) slen xs
+
+-- | Like 'slidingBlock' but expects the length of the list as first argument.
+slidingBlock' :: Int -> Int -> [a] -> [[a]]
+slidingBlock' len slen
+    | slen < 0    =  error "slidingBlock: negative block length"
+    | slen > len  =  error "slidingBlock: block longer than list"
+    | otherwise   =  partA (slen) (len-slen) 1
+
+-- @'slidingBlock'' len slen xs@ works as follows:
+-- Let's say @xs@ is given by
+--      1,2,...,n;
+-- first the list is (notationally) split into three parts, depending
+-- on the length @slen@ of the sliding block. We let @ka = len-slen@
+-- and @kb = slen@ and split the list like this:
+-- Case: slen >= len `div` 2
+--       part A  |   part B    |   part C
+--      1,...,ka | ka+1,...,kb | kb+1,...,n
+--   Then the functions partA, partB and partC treat each part accordingly
+--   to obtain our desired blocks:
+--      1,...,kb,
+--      2,...,kb+1
+--      and so on, up to
+--      ka+1,...,n.
+-- Case: slen < len `div` 2
+--       part A  |   part B    |   part C
+--      1,...,kb | kb+1,...,ka | ka+1,...,n
+--   Then partA, partB' and partC treat each part accordingly to obtain the
+--   same blocks as in the first case.
+--
+-- In the code below, ka and kb are actually used as counters:
+-- ">=" case: partA is finished as soon as ka == 0 and
+--            partB needs to take kb steps;
+-- "<"  case: partA is finished as soon as kb == 0 and
+--            partB' needs to take ka steps.
+
+
+-- treats part A;
+-- note that kb+j == slen+1 at any time
+-- (when called correctly from slidingBlock')
+partA :: Int -> Int -> Int -> [a] -> [[a]]
+partA kb  0 _ xs     = partB kb xs
+partA  0 ka j xs     = partB' (j-1) ka xs
+partA kb ka j (x:xs) =
+    mapFirstN l (x:) $ partA (kb-1) (ka-1) (j+1) xs
+        where
+            l = min (kb+j-1) j
+
+-- treats part B in the ">=" case
+partB :: Int -> [a] -> [[a]]
+partB  0 xs     = partC xs
+partB kb (x:xs) =
+    map (x:) (partB (kb-1) xs)
+
+-- treats part B in the "<" case
+partB' :: Int -> Int -> [a] -> [[a]]
+partB' l  0 xs     = partC xs
+partB' l kb (x:xs) =
+    [] : mapFirstN l (x:) (partB' l (kb-1) xs)
+
+-- treats part C
+partC :: [a] -> [[a]]
+partC = inits
